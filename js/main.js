bba0fcdaec981241f5176585da8de5899a67a1a7
Miyolophone <63515164+Miyolophone@users.noreply.github.com>
2024-10-16 01:13:49 -0400

    more consistent and prettier ui

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
 //RANDOM TEXT AND LOGO BACK
-const flavortext = new Array(
-    "<p>you're here early...!</p><p>the show will begin shortly.</p>",
-    "<p>i'll be ready in a minute!</p><p>don't go anywhere, okay?</p>",
-    "<p>wow, you're here already?</p><p>you just couldn't wait to see me, huh...</p>",
-    "<p>we're both pretty early...</p><p>b-but i was here first, okay!?</p>"
+const flavortextArr = new Array(
+    ["<p>you're here early...!</p>", "<p>the show will begin shortly.</p>"],
+    ["<p>i'll be ready in a minute!</p>","<p>don't go anywhere, okay?</p>"],
+    ["<p>wow, you're here already?</p>", "<p>you just couldn't wait to see me, huh...</p>"],
+    ["<p>we're both pretty early...</p>", "<p>b-but i was here first, okay!?</p>"]
 );
 
-const backurls = new Array(
+const backurlsArr = new Array(
     "../img/logoback1.png",
     "../img/logoback2.png",
     "../img/logoback3.png",
     "../img/logoback4.png",
-    "../img/logoback5.png"
 );
 
+const flavorTextSeed = Math.round(Math.random() * 100) % flavortextArr.length;
+document.querySelector(".innerft1").innerHTML = flavortextArr[flavorTextSeed][0];
+document.querySelector(".innerft2").innerHTML = flavortextArr[flavorTextSeed][1];
 
-document.querySelector(".flavor-text").innerHTML = flavortext[
-    Math.round(Math.random() * 100) % flavortext.length
-];
-document.querySelector(".back").style.setProperty("--randomBG", "url(" + backurls[
-    Math.round(Math.random() * 100) % backurls.length]
+document.querySelector(".back").style.setProperty("--randomBG", "url(" + backurlsArr[
+    Math.round(Math.random() * 100) % backurlsArr.length]
 + ")");
 
 
@@ -28,21 +27,21 @@ const logo = document.querySelector(".home-logo-image");
 const originalClass = logo.className;
 var isHovering = false;
 var isSpinning = false;
-var logoHeight = 19;
-var zoomFactor = 2;
-logo.style.setProperty("--height", logoHeight + "rem");
+var logoHeight = 310;
+var zoomFactor = 30;
+logo.style.setProperty("--height", logoHeight + "px");
 
 //checks if hovering over logo
 logo.addEventListener("mouseover", event => {
     isHovering = true;
-    logo.style.setProperty("--height", logoHeight + zoomFactor + "rem");
+    logo.style.setProperty("--height", logoHeight + zoomFactor + "px");
 });
 logo.addEventListener("mouseout", event => {
     isHovering = false;
     logo.style.setProperty("--rotateX", 0);
     logo.style.setProperty("--rotateY", 0);
     if (!isSpinning) {
-        logo.style.setProperty("--height", logoHeight + "rem");
+        logo.style.setProperty("--height", logoHeight + "px");
     }
 });
 
@@ -57,12 +56,12 @@ logo.addEventListener("click", event => {
         logo.className = originalClass;
         //re-randomize z rotation and bg image
         r = Math.round(Math.random() * 20) - 10;
-        document.querySelector(".back").style.setProperty("--randomBG", "url(" + backurls[
-            Math.round(Math.random() * 100) % backurls.length]
+        document.querySelector(".back").style.setProperty("--randomBG", "url(" + backurlsArr[
+            Math.round(Math.random() * 100) % backurlsArr.length]
         + ")");
         isSpinning = false;
         if (!isHovering) {
-            logo.style.setProperty("--height", logoHeight + "rem");
+            logo.style.setProperty("--height", logoHeight + "px");
         }
     });
 });
@@ -92,13 +91,12 @@ function rotateElement(event, element) {
         6);
 
 
-    //find offset from center, scaled to max rotation in degrees.
-    const offsetX = ((x - middleX)/middleX) * 80 * coeff;
-    const offsetY = ((y - middleY)/middleY) * 80 * coeff;
+    /* find offset from center and scales it to yield an appropriate value in degrees.
+    Clamps the maximum rotation value to 50 degrees */
+    const offsetX = Math.min(((x - middleX)/middleX) * 100 * coeff, 50);
+    const offsetY = Math.min(((y - middleY)/middleY) * 100 * coeff, 50);
 
 
     element.style.setProperty("--rotateX", offsetX + "deg");
     element.style.setProperty("--rotateY", -1* offsetY + "deg");
-
-
 }
